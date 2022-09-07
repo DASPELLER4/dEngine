4f35b1b5a4df764f09ea78627f396292a91fe216
DASPELLER4 <51001942+DASPELLER4@users.noreply.github.com>
2022-09-07 20:13:45 +0100

    Added string library
    Using linked lists for my strings will help ease up on the static memory allocation i used on my first attempt, where my old code would be output[4000] where 4000 is chose arbitrarily, now the size will always match the length of the string

---
+++ include/string.h
@@ -0,0 +1,96 @@
+#ifndef M_STRING_H
+#define M_STRING_H
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct string{
+	char value;
+	struct string *next;
+} string_t;
+
+string_t *findLastCharacter(string_t *string){
+	string_t *curr = string;
+	while(curr->next){
+		curr = curr->next;		
+	}
+	return curr;
+}
+
+void appendCharacter(string_t *string, char ch){
+	if(string){
+		string_t *last = findLastCharacter(string);
+		string_t *new = malloc(sizeof(string_t));
+		last->next = new;
+		new->value = ch;
+		new->next = NULL;
+	} else {
+		string = malloc(sizeof(string_t));
+		string->value = ch;
+		string->next = NULL;
+	}
+}
+
+void freeString(string_t *string){
+	if(string){
+		string_t *curr = string;
+		string_t *next;
+		while(curr){
+			next = curr->next;
+			free(curr);
+			curr = next;
+		}
+	}
+}
+
+void appendCharPtr(char *source, string_t *dest, short ifToFreeOrNotToFreeThatIsTheQuestion){
+	int len = strlen(source);
+	for(int i = 0; i < len; i++){
+		appendCharacter(dest, source[i]);
+	}
+	if(ifToFreeOrNotToFreeThatIsTheQuestion){
+		free(source);	
+	}
+} 
+
+void appendString(string_t *source, string_t *dest){
+	findLastCharacter(dest)->next = source;
+}
+
+string_t *defineEmptyString(){
+	return NULL;
+}
+
+string_t *defineStringFromCharPtr(char *start, short ifToFreeOrNotToFreeThatIsTheQuestion){
+	string_t *string = malloc(sizeof(string_t));
+	string->value = start[0];
+	string->next = NULL;
+	start++; // woop woop finally get to use pointer arithmetic
+	appendCharPtr(start, string, ifToFreeOrNotToFreeThatIsTheQuestion);
+	if(!ifToFreeOrNotToFreeThatIsTheQuestion)
+		start--;
+	return string;
+}
+
+int stringLen(string_t *string){
+	string_t *curr = string;
+	int i = 0;
+	while(curr){
+		i++;
+		curr = curr->next;
+	}
+	return i;
+}
+
+char *typeCastToCharPtr(string_t *string){
+	char *ret = calloc(stringLen(string)+1, 1);
+	string_t *curr = string;
+	int i;
+	for(i = 0; curr; i++){
+		ret[i] = curr->value;
+		curr = curr->next;
+	}
+	return ret;
+}
+
+#endif
