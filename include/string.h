b34d6b9495494a562626186a4859b76116c5ce0b
DASPELLER4 <51001942+DASPELLER4@users.noreply.github.com>
2022-09-08 18:28:31 +0100

    fixed pointer issue

--- include/string.h
+++ include/string.h
@@ -43,10 +43,10 @@ void freeString(string_t *string){
 	}
 }
 
-void appendCharPtr(char *source, string_t *dest, short ifToFreeOrNotToFreeThatIsTheQuestion){
+void appendCharPtr(char *source, string_t **dest, short ifToFreeOrNotToFreeThatIsTheQuestion){
 	int len = strlen(source);
 	for(int i = 0; i < len; i++){
-		appendCharacter(&dest, source[i]);
+		appendCharacter(dest, source[i]);
 	}
 	if(ifToFreeOrNotToFreeThatIsTheQuestion){
 		free(source);	
@@ -63,7 +63,7 @@ string_t *defineEmptyString(){
 
 string_t *defineStringFromCharPtr(char *start, short ifToFreeOrNotToFreeThatIsTheQuestion){
 	string_t *string = NULL;
-	appendCharPtr(start, string, ifToFreeOrNotToFreeThatIsTheQuestion);
+	appendCharPtr(start, &string, ifToFreeOrNotToFreeThatIsTheQuestion);
 	if(ifToFreeOrNotToFreeThatIsTheQuestion){
 		free(start);
 	}
