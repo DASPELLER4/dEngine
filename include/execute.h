6235961069f45e654ac128e61d0ff1f01eca4168
DASPELLER4 <51001942+DASPELLER4@users.noreply.github.com>
2022-09-10 19:27:00 +0100

    Add files via upload

--- include/execute.h
+++ include/execute.h
@@ -10,6 +10,7 @@
 #include "string.h"
 #include "split.h"
 #include "../config.h"
+#include "polyEditor.h"
 
 #if defined(USE_VIM)
 	#define EDITOR "vim "
@@ -23,9 +24,9 @@ void execute(split_t* args){
 	// HUGE IF STATEMENT FOR ALL COMMANDS
 	static char projectFolder[512] = {0};
 	if(strcmp(args->splits[0],"help") == 0){
-		printf("HELP PAGE\nhelp - shows this page\nquit - quits the program\nproject - opens/creates project folder\nscript - edits existing script file (see object command to create script)\n");
+		printf("HELP PAGE\nhelp - shows this page\nquit - quits the program\nproject - opens/creates project folder\nscript - edits existing script file (see object command to create script)\nlist - lists things (use list help for more info)\n");
 	} else if(strcmp(args->splits[0],"project") == 0){
-		if(args->length == 2){
+		if(args->length >= 2){
 			struct stat check;
 			if (stat(args->splits[1], &check) == 0 && S_ISDIR(check.st_mode)) {
 				printf("Environment Set to %s\n", args->splits[1]);
@@ -49,7 +50,7 @@ void execute(split_t* args){
 	} else if(strcmp(args->splits[0],"script") == 0){
 		if(!projectFolder[0]){
 			printf("Project not opened, use the project command to open/create a project\n");
-		} else if(args->length == 2){
+		} else if(args->length >= 2){
 			chdir(projectFolder);
 			chdir("Scripts");
 			string_t *fileName = defineStringFromCharPtr(args->splits[1], 0);
@@ -73,6 +74,87 @@ void execute(split_t* args){
 		} else {
 			printf("Argument [object name] expected\n");
 		}
+	} else if(strcmp(args->splits[0],"list") == 0){
+		if(!projectFolder[0]){
+			printf("Project not opened, use the project command to open/create a project\n");
+		} else if(args->length >= 2){
+			if(strcmp(args->splits[1],"help") == 0){
+				printf("List help\nOptions:\nall - list all files\nscripts - list all user scripts\nobjects - list all objects\n");
+			} else if(strcmp(args->splits[1],"all") == 0){
+				chdir(projectFolder);
+				printf("Scripts:\n");
+				system("ls Scripts | paste -d '' - /dev/null");
+				printf("Objects:\n");
+				system("ls Objects | paste -d '' - /dev/null");
+				printf("Tools:\n");
+				system("ls Tools | paste -d '' - /dev/null");
+				chdir("../");
+			} else if(strcmp(args->splits[1],"scripts") == 0){
+				chdir(projectFolder);
+				printf("Scripts:\n");
+				system("ls Scripts | paste -d '' - /dev/null");
+				chdir("../");
+			} else if(strcmp(args->splits[1],"objects") == 0){
+				chdir(projectFolder);
+				printf("Objects:\n");
+				system("ls Objects | paste -d '' - /dev/null");
+				chdir("../");
+			} else
+				printf("Unknown argument to command list: %s\nUse list help to list possible arguments\n", args->splits[1]);
+		} else
+			printf("Argument expected - use list help to see all available options\n");
+	} else if(strcmp(args->splits[0],"object") == 0){
+		if(!projectFolder[0]){
+			printf("Project not opened, use the project command to open/create a project\n");
+		} else if(args->length >= 2){
+			int valid = 1;
+			for(int i = 0; args->splits[1][i]; i++){
+				if(args->splits[1][i] > 'z' || args->splits[1][i] < 'a')
+					valid = 0;
+			}
+			if(valid){
+				chdir(projectFolder);
+				chdir("Objects");
+				int brightness = atoi(args->splits[2]);
+				if(brightness > 5 || brightness < 1){
+					printf("Brightness has to be 1-5\n");
+				} else {
+					eObject_t *outputObject = polyEditor(brightness);
+					FILE *fp = fopen(args->splits[1], "w");
+					fprintf(fp, "");
+					fclose(fp);
+					fp = fopen(args->splits[1], "a+");
+					int lowestX, lowestY;
+					lowestX = OBJECTSCALE;
+					lowestY = OBJECTSCALE;
+					for(int i = 0; i<outputObject->count; i++){
+						for(int j = 0; j<3; j++){
+							if(gobject->polygons[i].v[j].a < lowestX)
+								lowestX = gobject->polygons[i].v[j].a;
+							if(gobject->polygons[i].v[j].b < lowestY)
+								lowestY = gobject->polygons[i].v[j].b;
+						}
+					}
+					for(int i = 0; i<outputObject->count; i++){
+						fprintf(fp, "%d %d %d %d %d %d %c\n", (int)(gobject->polygons[i].v[0].a-lowestX), (int)(gobject->polygons[i].v[0].b-lowestY), (int)(gobject->polygons[i].v[1].a-lowestX), (int)(gobject->polygons[i].v[1].b-lowestY), (int)(gobject->polygons[i].v[2].a-lowestX), (int)(gobject->polygons[i].v[2].b-lowestY), gobject->brightness-1);
+					}
+					fclose(fp);
+					chdir("../Scripts");
+					string_t *scriptName = defineStringFromCharPtr(args->splits[1], 0);
+					appendCharPtr(".h", &scriptName, 0);
+					char *charScriptName = typeCastToCharPtr(scriptName);
+					fp = fopen(charScriptName,"w");
+					fclose(fp);
+					freeString(scriptName);
+					free(charScriptName);
+					free(outputObject);
+				}
+				chdir("../..");
+			} else{
+				printf("Name of object cannot include anything other than lowercase alphabetical characters\n");
+			}
+		} else
+			printf("Argument expected [object name]\n");
 	} else if(strcmp(args->splits[0],"quit") == 0){
 	} else {
 		printf("Command not recognized - run help to see all available commands\n");
---
+++ include/polyEditor.h
@@ -0,0 +1,142 @@
+#ifndef POLYEDITOR_H
+#define POLYEDITOR_H
+
+#define ELENOFLINE 64
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/freeglut.h>
+
+#include "../config.h"
+
+typedef struct eVector2d{
+	double a;
+	double b;
+} eVector2d_t;
+
+typedef struct ePolygon{
+	eVector2d_t v[3];
+} ePolygon_t;
+
+typedef struct eObject{
+	int count;
+	int brightness;
+	ePolygon_t polygons[MAXPOLYGONS];
+} eObject_t;
+
+eObject_t *gobject;
+int *gsize;
+
+void addVert(int x, int y, short clear){
+	static int currVert = 0;
+	if(clear){
+		currVert = 0;
+		return;
+	}
+	if(gobject->count >= MAXPOLYGONS-1){
+		printf("You have exceeded the maximum polygons available, to support larger object complexity\nIncrease the MAXPOLYGONS macro in config.h and re-run make\n");
+		return;
+	}
+	gobject->polygons[gobject->count].v[currVert].a = ((double)x)/((double)glutGet(GLUT_WINDOW_WIDTH));
+	gobject->polygons[gobject->count].v[currVert].b = ((double)y)/((double)glutGet(GLUT_WINDOW_HEIGHT));
+	currVert+=1;
+	if(currVert>=3){
+		currVert = 0;
+		gobject->count+=1;
+	}
+}
+
+int removePoly(){
+	gobject->count -= 1;
+	if(gobject->count < 0)
+		gobject->count = 0;
+	return gobject->count;
+}
+
+void mouseClicks(int button, int state, int x, int y){
+	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+		addVert(x, y, 0);
+	}else if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN){
+		removePoly();
+	}
+}
+
+void keypress(unsigned char key, int x, int y){
+	switch(key){
+		case('q'):
+			glutDestroyWindow(glutGetWindow());
+			break;
+		case('r'):
+			while(removePoly());
+			addVert(x, y, 1);
+			break;
+		case('h'):
+			printf("Object Generator Help Page\n\tThis tool is to assist in the creation of a 2D object\n\nControls:\n\tLMB-Places a new vertex, takes 3 vertecies to create a polygon\n\tRMB-Delete last polygon\n\tQ-Save and quit the software\n\tR-Reset all polygons\n\tH-Display this page\n");
+			break;
+		default:
+			printf("Unrecognized keypress (\'%c\').\nPress H to show the help page\n", key);
+	}
+}
+
+void idle(){
+	glutPostRedisplay();
+}
+
+void display(){
+	gsize[0] = glutGet(GLUT_WINDOW_WIDTH);
+	gsize[1] = glutGet(GLUT_WINDOW_HEIGHT);
+	glClear(GL_COLOR_BUFFER_BIT);
+	for(int i = 0; i<gobject->count; i++){
+		float stdBrightness = ((float)gobject->brightness)/5.0f;
+		glColor3f(stdBrightness, stdBrightness, stdBrightness);
+		glBegin(GL_POLYGON);
+			glVertex3f(gobject->polygons[i].v[0].a*2-1, -(gobject->polygons[i].v[0].b*2-1), 0);
+			glVertex3f(gobject->polygons[i].v[1].a*2-1, -(gobject->polygons[i].v[1].b*2-1), 0);
+			glVertex3f(gobject->polygons[i].v[2].a*2-1, -(gobject->polygons[i].v[2].b*2-1), 0);
+		glEnd();
+	}
+	glFlush();
+}
+
+eObject_t *polyEditor(int brightness){
+	char intToCharBrightness[5] = {'.','<','|','X','$'};
+	gobject = calloc(1,sizeof(eObject_t));
+	gobject->brightness = brightness;
+	gsize = malloc(2*sizeof(int));
+	//make up the args for glutInit
+	int x = 1;
+	char **j = malloc(sizeof(char*));
+	j[0] = malloc(2);
+	j[0][0] = 'a';
+	j[0][1] = 0;
+	printf("Object Generator Help Page\n\tThis tool is to assist in the creation of a 2D object\n\nControls:\n\tLMB-Places a new vertex, takes 3 vertecies to create a polygon\n\tRMB-Delete last polygon\n\tQ-Save and quit the software\n\tR-Reset all polygons\n\tH-Display this page\n");
+	//start opengl
+	glutInit(&x, j);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(80, 80);
+	glutInitWindowSize(400, 300);
+	glutCreateWindow("Object Creator | h for Help");
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutKeyboardFunc(keypress);
+	glutMouseFunc(mouseClicks);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutMainLoop();
+	free(j[0]);
+	free(j);
+	//formulate response
+	for(int i = 0; i < gobject->count; i++){
+		gobject->polygons[i].v[0].a *= OBJECTSCALE;
+		gobject->polygons[i].v[0].b *= OBJECTSCALE;
+		gobject->polygons[i].v[1].a *= OBJECTSCALE;
+		gobject->polygons[i].v[1].b *= OBJECTSCALE;
+		gobject->polygons[i].v[2].a *= OBJECTSCALE;
+		gobject->polygons[i].v[2].b *= OBJECTSCALE;
+	}
+	free(gsize);
+	gobject->brightness = intToCharBrightness[gobject->brightness-1];
+	return gobject; 
+}
+
+#endif
